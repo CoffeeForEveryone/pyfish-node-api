54056046dda0cbfdb303ec343eb6abd597d5089c
I'mPSK <32847514+kongsaza1133@users.noreply.github.com>
2022-09-02 18:39:17 +0700

    change format login

--- route/operation.js
+++ route/operation.js
@@ -34,12 +34,11 @@ route.post('/',(req,res)=>{
                 }
             })
 
-            conn.query('UPDATE rent_data SET rent_status = 0 WHERE (user_mac = ? && rent_status = ?) && (DATE(NOW()) BETWEEN rent_start AND rent_end)',[mac,1],(err,result,field)=>{
+            conn.query('UPDATE rent_data SET rent_status = 0 WHERE (user_mac = ? && rent_status = ?) && (DATE(NOW()) NOT BETWEEN rent_start AND rent_end)',[mac,1],(err,result,field)=>{
                 if(err){
                     res.status(400).json({"msg":"Update Failed : "+ err,"status":"false"})
                     return
                 }
-
                 conn.query('SELECT * FROM rent_data WHERE (user_mac = ? && function_id = ?) && rent_status = ?',[mac,function_id,1],(err,result,field)=>{
                     
                     if(err){
@@ -51,7 +50,7 @@ route.post('/',(req,res)=>{
                     }
                     let privilage = moment().isBefore(result[0].rent_end)
                     let status = moment(result[0].rent_end).fromNow()
-                    let rent_end = moment(result[0].rent_end).add(543,'year').format('LLL')
+                    let rent_end = moment(result[0].rent_end).add(543,'year').format('D MMMM YYYY HH:mm:ss ')
                     res.json({
                         "can_access":privilage,
                         "time_from_now":status,
